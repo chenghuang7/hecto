d98b7dba79978077ef2b4d50073aa35414fcd138
xushouyi <1776007548@qq.com>
2024-04-02 15:11:21 +0800

    find finish

--- src/editor.rs
+++ src/editor.rs
@@ -60,7 +60,8 @@ impl Editor {
 
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
-        let mut initial_status = String::from("HELP: Ctrl-A = quit");
+        let mut initial_status =
+            String::from("HELP: Ctrl-F = find | Ctrl-S = save | Ctrl-A = quit");
         let document = if args.len() > 1 {
             let file_name = &args[1];
             // Document::open(&file_name).unwrap_or_default()
@@ -123,6 +124,23 @@ impl Editor {
             }
             Key::Ctrl('s') => {
                 self.save();
+            }
+            Key::Ctrl('f') => {
+                if let Some(query) = self
+                    .prompt("Search: ", |editor, _, query| {
+                        if let Some(position) = editor.document.find(&query) {
+                            editor.cursor_pos = position;
+                            editor.scroll();
+                        }
+                    })
+                    .unwrap_or(None)
+                {
+                    if let Some(position) = self.document.find(&query[..]) {
+                        self.cursor_pos = position;
+                    } else {
+                        self.status_msg = StatusMessage::from("Not found".to_string());
+                    }
+                }
             }
             Key::Insert => {}
             Key::Up
@@ -360,12 +378,16 @@ impl Editor {
         }
     }
 
-    fn prompt(&mut self, prompt: &str) -> Result<Option<String>, std::io::Error> {
+    fn prompt<C>(&mut self, prompt: &str, callback: C) -> Result<Option<String>, std::io::Error>
+    where
+        C: Fn(&mut Self, Key, &String),
+    {
         let mut result = String::new();
         loop {
             self.status_msg = StatusMessage::from(format!("{}{}", prompt, result));
             self.refresh_screen()?;
-            match Terminal::read_key()? {
+            let key = Terminal::read_key()?;
+            match key {
                 Key::Backspace => {
                     if !result.is_empty() {
                         result.truncate(result.len() - 1);
@@ -377,8 +399,13 @@ impl Editor {
                         result.push(c);
                     }
                 }
+                Key::Esc => {
+                    result.truncate(0);
+                    break;
+                }
                 _ => (),
             }
+            callback(self, key, &result);
         }
         self.status_msg = StatusMessage::from(String::new());
         if result.is_empty() {
@@ -389,7 +416,8 @@ impl Editor {
 
     fn save(&mut self) {
         if self.document.filename.is_none() {
-            let new_name = self.prompt("Save as: ").unwrap_or(None);
+            // let new_name = self.prompt("Save as: ").unwrap_or(None);
+            let new_name = self.prompt("Save as: ", |_, _, _| {}).unwrap_or(None);
             if new_name.is_none() {
                 self.status_msg = StatusMessage::from("Save aborted.".to_string());
                 return;
